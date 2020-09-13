2b466282087273b4919c0e17d72b6a6586d004c6
JoBite1235 <63899068+JoBite1235@users.noreply.github.com>
2020-09-13 18:33:45 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-document.body.addEventListener("mousemove", evt => {
+document.body.addEventListener("mousemove", (evt) => {
   const mouseX = evt.clientX;
   const mouseY = evt.clientY;
-
-  gsap.set(".cursor", {
-    x: mouseX,
-    y: mouseY });
-
 
   gsap.to(".shape", {
     x: mouseX,
     y: mouseY,
-    stagger: -0.1 });
-
+    stagger: -0.1,
+  });
 });
